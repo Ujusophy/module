b6af998582a2312e4b7f65c29660631869da18e6
Njoku Ujunwa Sophia <106748148+Ujusophy@users.noreply.github.com>
2024-09-12 13:51:27 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform { 
+  cloud { 
+    
+    organization = "techynurse" 
+
+    workspaces { 
+      name = "ec2-instance-deployment" 
+    } 
+  } 
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2-instance"
+  instance_type  = var.instance_type
+  region = var.region
+}
+
+variable "region" {
+  description = "The AWS region to deploy the instance in"
+  default      = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type"
+  default     = "t2.micro"
+}
